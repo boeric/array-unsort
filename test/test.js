a523f3fcb575a2ed05c7cc11847df4420f5ac9d9
Bo Ericsson <bo@boe.net>
2020-05-25 10:10:44 -0700

    Test

--- test/test.js
+++ test/test.js
@@ -126,11 +126,8 @@ describe('Algorithm', () => {
       const length = 5;
       const map = {};
       // To test for true randomness is complicated (https://en.wikipedia.org/wiki/Diehard_tests)
-      // Can't do that here, so we're using something very simplified, perhaps incorrect...
-      const threshold = 0.003;
-      const threshold2 = Math.log(iterations) / 10 / length;
-      const threshold3 = Math.log(iterations) / Math.LOG10E;
-      const threshold4 = Math.LOG10E / Math.log(iterations);
+      // Can't do that here, so we're using something very simplified
+      const threshold = 0.03;
 
       // Init map
       range(length).forEach((i) => {
@@ -151,28 +148,21 @@ describe('Algorithm', () => {
       });
 
       // Process map and determine the array elems with highest (max) and lowest (min) frequency
-      let max = -Infinity;
-      let min = Infinity;
+      let freqMax = -Infinity;
+      let freqMin = Infinity;
       Object.keys(map).forEach((key) => {
         Object.keys(map[key]).forEach((pos) => {
           const count = map[key][pos];
-          const pct = count / iterations;
-          if (pct < min) min = pct;
-          if (pct > max) max = pct;
+          const freq = count / iterations;
+          freqMin = (freq < freqMin) ? freq : freqMin;
+          freqMax = (freq > freqMax) ? freq : freqMax;
         });
       });
 
-      // Determine the expected frequency
-      const expected = 1 / length;
-
-      // Determine the difference between the expected frequency and the max/min numbers found
-      const maxDiff = max - expected;
-      const minDiff = expected - min;
-      // console.log(maxDiff, minDiff, threshold, maxDiff - minDiff);
-      console.log(max, min, max - min, threshold, threshold2, threshold3, threshold4);
-      console.log('map', map)
-
-      return (maxDiff < threshold && minDiff < threshold) || false;
+      // With an array length of x, the count of each bin should approach 1 / x for a random
+      // distribution as the iteration count increases. Here we're accepting a frequency
+      // difference of 3 percent for 10k iterations.
+      return Math.abs(freqMax - freqMin) < threshold;
     }
 
     it('should generate random output index for each input index', () => {
